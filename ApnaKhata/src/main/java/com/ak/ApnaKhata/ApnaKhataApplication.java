dad1c4adca9937e2ac7c566594140680dd4ca86a
javas <javas@LAPTOP-A6Q1QVS3>
2021-05-02 12:01:22 +0530

    first commit

---
+++ ApnaKhata/src/main/java/com/ak/ApnaKhata/ApnaKhataApplication.java
@@ -0,0 +1,13 @@
+package com.ak.ApnaKhata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApnaKhataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApnaKhataApplication.class, args);
+	}
+
+}

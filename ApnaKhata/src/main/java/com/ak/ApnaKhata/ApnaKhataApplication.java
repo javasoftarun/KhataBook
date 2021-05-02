8581dcb039fc6357295362183b95ec01094a6d21
Dell <Dell@DESKTOP-5HEVMRH>
2021-05-02 12:21:03 +0530

    Wecome message

--- ApnaKhata/src/main/java/com/ak/ApnaKhata/ApnaKhataApplication.java
+++ ApnaKhata/src/main/java/com/ak/ApnaKhata/ApnaKhataApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApnaKhataApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to ApnaKhata");
 		SpringApplication.run(ApnaKhataApplication.class, args);
 	}
 

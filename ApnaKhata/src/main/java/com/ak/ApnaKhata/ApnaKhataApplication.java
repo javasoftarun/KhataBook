105c956c168f5a4bc3fa10ebf0d08db2c0eef8ec
javasoftarun <javasoftarun159@gmail.com>
2023-03-02 12:24:19 +0530

    test

--- ApnaKhata/src/main/java/com/ak/ApnaKhata/ApnaKhataApplication.java
+++ ApnaKhata/src/main/java/com/ak/ApnaKhata/ApnaKhataApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApnaKhataApplication {
-	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApnaKhataApplication.class, args);
 		System.out.println("Hi");
+		System.out.println("hello");
 	}
 
 }

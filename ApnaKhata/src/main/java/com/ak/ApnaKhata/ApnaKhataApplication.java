c28381bf4c308e291fba61c1836f83b05f3d5808
javasoftarun <javasoftarun159@gmail.com>
2023-03-02 12:19:48 +0530

    test

--- ApnaKhata/src/main/java/com/ak/ApnaKhata/ApnaKhataApplication.java
+++ ApnaKhata/src/main/java/com/ak/ApnaKhata/ApnaKhataApplication.java
@@ -8,6 +8,8 @@ public class ApnaKhataApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApnaKhataApplication.class, args);
+		System.out.println("hellow");
 	}
+	
 
 }
